f486e60f08025ee04f3ccf20e951f23e93cb2dda
Basti <sebastian.hedwig@web.de>
2026-02-15 12:26:01 +0100

    init commit: add projekt basestructure

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+
+const srcPath = fileURLToPath(new URL('./src', import.meta.url));
+const assetsPath = fileURLToPath(new URL('./src/assets', import.meta.url));
+const scssPath = fileURLToPath(new URL('./src/scss', import.meta.url));
+
+export default defineConfig({
+  base: "/memory/",
+  resolve: {
+    alias: {
+      '@': srcPath,
+      '@assets': assetsPath,
+      '@scss': scssPath
+    }
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        loadPaths: [scssPath],
+        additionalData: `
+          @use "abstracts/variables" as *;
+          @use "abstracts/mixins" as *;
+        `
+      }
+    }
+  },
+
+  server: {
+    open: true,
+    port: 5173
+  },
+
+  build: {
+    outDir: "dist",
+    sourcemap: true
+  }
+});
