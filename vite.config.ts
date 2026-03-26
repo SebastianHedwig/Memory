5bec1a905b9d8809584b91897247f7db77c71e95
Basti <sebastian.hedwig@web.de>
2026-03-26 22:59:48 +0100

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import { fileURLToPath, URL } from 'node:url';
 const srcPath = fileURLToPath(new URL('./src', import.meta.url));
 const assetsPath = fileURLToPath(new URL('./src/assets', import.meta.url));
 const scssPath = fileURLToPath(new URL('./src/scss', import.meta.url));
+const indexHtml = fileURLToPath(new URL('./index.html', import.meta.url));
+const settingsHtml = fileURLToPath(new URL('./settings.html', import.meta.url));
+const gameHtml = fileURLToPath(new URL('./game.html', import.meta.url));
+const legalNoticeHtml = fileURLToPath(new URL('./legal-notice.html', import.meta.url));
+const privacyPolicyHtml = fileURLToPath(new URL('./privacy-policy.html', import.meta.url));
 
 export default defineConfig({
   base: "/memory/",
@@ -30,6 +35,15 @@ export default defineConfig({
 
   build: {
     outDir: "dist",
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: indexHtml,
+        settings: settingsHtml,
+        game: gameHtml,
+        "legal-notice": legalNoticeHtml,
+        "privacy-policy": privacyPolicyHtml
+      }
+    }
   }
 });
